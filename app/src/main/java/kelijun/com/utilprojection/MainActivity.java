e1a3839e6146b99136331354f2583e091c0a669c
kelijun <447857062@qq.com>
2018-06-14 17:34:43 +0800

    提交

--- app/src/main/java/kelijun/com/utilprojection/MainActivity.java
+++ app/src/main/java/kelijun/com/utilprojection/MainActivity.java
@@ -1,7 +1,12 @@
 package kelijun.com.utilprojection;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import q.rorbin.badgeview.QBadgeView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textview = (TextView) findViewById(R.id.textView);
+        ConstraintLayout layout= (ConstraintLayout) findViewById(R.id.ConstraintLayout);
+        new QBadgeView(this)
+                .bindTarget(layout)
+                .setBadgeGravity(Gravity.END|Gravity.TOP)
+                .setBadgeNumber(12);
     }
 }
